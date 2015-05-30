4e28b74559a64b7901a9beaaf1c9068be3690eee
Benjamin R Sergent <BenjaminRSergent@gmail.com>
2015-05-30 15:56:17 -0700

    Started Project from libgdxsetup.jar

---
+++ desktop/src/com/developworlds/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.developworlds.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.developworlds.FlockingApplication;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlockingApplication(), config);
+	}
+}
